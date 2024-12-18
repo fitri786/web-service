e3e3f8fdb39e166c04d4f14dcf227ab84f3e18b3
fitri786 <fitrisrimlyn10@gmail.com>
2024-12-18 12:57:55 +0700

    Baru Update 18 12 2024 1257

--- src/server/handler.js
+++ src/server/handler.js
@@ -6,48 +6,51 @@ const { Firestore } = require('@google-cloud/firestore');
 async function postPredictHandler(request, h) {
   const { image } = request.payload;
   const { model } = request.server.app;
-
- 
-  const id = crypto.randomUUID();
-  const createdAt = new Date().toISOString();
-  const data = {
-    id,
-    result: null,
-    suggestion: null,
-    createdAt,
-  };
-
   
   const { result, suggestion } = await predictClassification(model, image);
-  data.result = result;
-  data.suggestion = suggestion;
+  const id = crypto.randomUUID();
+  const createdAt = new Date().toISOString();
+
+  const data = {
+    "id": id,
+    "result": result,
+    "suggestion": suggestion,
+    "createdAt": createdAt
+}
 
   await storeData(id, data);
-
   
   const response = h.response({
     status: 'success',
     message: 'Model is predicted successfully',
-    data,
+    data: data
   });
+
   response.code(201);
   return response;
 }
 
 async function getHistoriesHandler(request, h) {
-  const db = new Firestore();
-  const predictCollection = db.collection('prediction');
-  const predictSnapshot = await predictCollection.get();
+  const db = new Firestore();                            
+  const predictCollection = db.collection('prediction'); 
+  const predictSnapshot = await predictCollection.get(); 
 
-  const data = predictSnapshot.docs.map(doc => ({
-    id: doc.id,
-    history: doc.data(),
-  }));
+  const data = [];
 
-  return h.response({
-    status: 'success',
-    data,
-  }).code(200);
+  predictSnapshot.forEach((doc) => {
+      const history = {
+          id: doc.id,
+          history: doc.data()
+      };
+      data.push(history);
+  });
+
+  const response = h.response({
+      status: 'success',
+      data: data
+  });
+  response.code(200);
+  return response;
 }
 
 module.exports = { postPredictHandler, getHistoriesHandler };
